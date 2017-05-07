361430cfd35745966e40bda671adbb12af9920ce
kelvin-williams <kelvin-williams@hotmail.com>
2017-05-07 15:18:21 -0300

    hm

--- BasicFunctions.h
+++ BasicFunctions.h
@@ -15,10 +15,12 @@ void DrawTriangle( glm::vec3 p1, glm::vec3 p2, glm::vec3 p3){
 
     glBegin(GL_TRIANGLES);
 
-    glColor3f(1.0f, 0.0f, 0.0);
+    glColor3f(1.0f, 0.0f, 0.0f);
 
     glVertex3f( p2.x, p2.y, p2.z);
+    glColor3f(0.0f, 1.0f, 0.0f);
     glVertex3f( p1.x, p1.y, p1.z);
+    glColor3f(0.0f, 0.0f, 1.0f);
     glVertex3f( p3.x, p3.y, p3.z);
 
     glEnd();
